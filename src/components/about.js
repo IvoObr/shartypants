1d971df2df6db914435f3cc92f26aabbb2d0197b
IvoObr <ivo_obr@hotmail.com>
2021-06-30 19:31:46 +0300

    about photos

--- src/components/about.js
+++ src/components/about.js
@@ -8,16 +8,25 @@ export default class About extends React.Component {
         return (
             <div id="aboutScreen" className="screen">
                 <h1 id="about" className="header">About Us</h1>
-                <div>
-                    <span>
-                        <img className="photo" src={rali} alt="rali" />
-                        <img className="photo" src={niki} alt="niki" />
-                    </span>
+                <div className="">
                     <p className="textCol">{about.text.p1}</p>
                     <p className="textCol">{about.text.p2}</p>
-                    <p className="textCol">{about.text.p3}</p>
-                    <p>{about.text.niki}</p>
-                    <p>{about.text.rali}</p>
+                </div>
+                <div className="people">
+                    <div className="aboutCol">
+                        <img className="photo" src={rali} alt="rali" />
+                        <div className="cen">
+                            <p className="name">Ralitsa Golemanova</p>
+                            <p className="h2sm">{about.text.rali}</p>
+                        </div>
+                    </div>
+                    <div className="aboutCol">
+                        <img className="photo" src={niki} alt="niki" />
+                        <div className="cen">
+                            <p className="name">Nikolay Stefanov</p>
+                            <p className="h2sm">{about.text.niki}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
