e478dc84e94334c96d542c3f94cc9e150e46d4e0
IvoObr <ivo_obr@hotmail.com>
2021-07-26 18:04:42 +0300

    rm slice css

--- src/components/home.js
+++ src/components/home.js
@@ -15,14 +15,6 @@ export default class Home extends React.Component {
                         <div className="glitch__item"></div>
                         <div className="glitch__item"></div>
                         <a href="#nav">
-                            {/* 
-                            POLYGON
-                            <div className="image-wrapper drop">
-                                <img className="image-1" alt="drop" src={drop} />
-                                <img className="image-2" alt="drop" src={drop} />
-                                <img className="image-3" alt="drop" src={drop} />
-                                <img className="image-4" alt="drop" src={drop} />
-                            </div> */}
                             <img className='drop' alt='sp' src={drop} />
                         </a>
                     </div>
--- src/css/animations/index.js
+++ src/css/animations/index.js
@@ -1,11 +1,9 @@
-import slice from './slice.css'
 import modal from './modal.css'
 import glitch from './glitch.css'
 import button from './button.css'
 import gallery from './gallery.css'
 
 export {
-    slice,
     modal,
     glitch,
     button,
